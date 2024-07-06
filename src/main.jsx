dc05f2218a51cc339d7ec0092c55d68aee779402
Hoang Bui Duc <hoangakiy@gmail.com>
2024-07-07 01:47:52 +0700

    feat: improt redux

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux';
-
+import store from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider>
+  <Provider store={store}>
+
     <React.StrictMode>
+
       <App />
+
     </React.StrictMode>
-  </Provider>
+
+  </Provider>,
 )
